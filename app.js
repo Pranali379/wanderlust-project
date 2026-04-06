7242bb90f6a8d6800375a0432784e15670132bb2
Pranali Khot <khotpranali77@gmail.com>
2026-04-06 10:10:21 +0530

    fixed session store crash

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
@@ -57,10 +57,14 @@ store.on("error", (err) => {
 
 // ------------------ SESSION CONFIG ------------------
 const sessionOptions = {
-  store,
-  secret: "mysupersecretcode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 
 app.use(session(sessionOptions));
